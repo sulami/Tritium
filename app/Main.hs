d7179a57521022dc857a7a8818367abf8eb898ee
Robin Schroer <sulami@peerwire.org>
2015-10-31 14:47:29 +0100

    initialize an opengl context

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,10 @@
 module Main where
 
-import Lib
+import           Graphics.UI.Fungen (RefreshType(Idle), colorMap, funInit)
 
 main :: IO ()
-main = someFunc
+main = do
+  let windowConfig = ((0,0), (800,600), "Tritium v0.1")
+      gameMap = colorMap 0.0 0.0 0.0 250 250
+  funInit windowConfig gameMap [] () () [] (return ()) Idle []
+
