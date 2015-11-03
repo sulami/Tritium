768f165fe0fb1105d15507e012ae01508b757d3c
Robin Schroer <sulami@peerwire.org>
2015-11-03 14:35:03 +0100

    add the core loop to react to events and redraw

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,11 @@
 module Main where
 
-import           SFML.Graphics.RenderWindow (createRenderWindow)
-import           SFML.Window.Types (Window)
+import           System.Exit (exitSuccess)
+
+import qualified SFML.Graphics.Color as COL
+import           SFML.Graphics.Types (RenderWindow)
+import qualified SFML.Graphics.RenderWindow as RW
+import           SFML.Window.Event (SFEvent (..))
 import           SFML.Window.VideoMode (VideoMode (..))
 import           SFML.Window.Window (WindowStyle (..))
 
@@ -14,6 +18,24 @@ main = do
       title = "Tritium"
       style = [SFTitlebar, SFClose]
       cnxst = Nothing
-  createRenderWindow vmode title style cnxst
-  return ()
+  window <- RW.createRenderWindow vmode title style cnxst
+  coreLoop window
+
+coreLoop :: RenderWindow -> IO ()
+coreLoop window = do
+  RW.clearRenderWindow window COL.black
+  event <- RW.pollEvent window
+  case event of
+    Just ev -> handleEvent window ev
+    Nothing -> return ()
+  RW.display window
+  coreLoop window
+
+handleEvent :: RenderWindow -> SFEvent -> IO ()
+handleEvent window event = do
+  case event of
+    SFEvtClosed -> do RW.close window
+                      RW.destroy window
+                      exitSuccess
+    _           -> return ()
 
